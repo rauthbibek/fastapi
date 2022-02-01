6b91e46b69ecf2641de563067849f2169d5dcd3d
Bibek Rauth <rauth.bibek007@gmail.com>
2022-02-01 23:15:01 +0530

    Pydantic with fastapi

---
+++ fastapi_test4.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+class Items(BaseModel):
+    name: str
+    price: float
+    description: Optional[str]= None
+    tax: Optional[float]= None
+
+
+app = FastAPI()
+
+@app.post('/items/')
+async def create_items(item: Items):
+    print(item)
+    return item
