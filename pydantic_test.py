ed6800e395fcf4681f47914c5501920c8408973f
Bibek Rauth <rauth.bibek007@gmail.com>
2022-02-01 21:17:47 +0530

    Pydantic

--- pydantic_test.py
+++ pydantic_test.py
@@ -11,7 +11,8 @@ class Users(BaseModel):
     timestamp : Optional[datetime] = None
     friends : List[int] = []
 
-data = {'id': '1234', 'username': 'wai foong', 'password': 'Password123', 'confirm_password': 'Password123', 'timestamp': '2020-08-03 10:30', 'friends': [1, '2', b'3']}
+data = {'id': '1234', 'username': 'wai foong', 'password': 'Password123',
+ 'confirm_password': 'Password123', 'timestamp': '2020-08-03 10:30', 'friends': [1, '2', b'3']}
 
 user = Users(**data)
 
